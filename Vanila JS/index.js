949a5efdb4f151290f67fbf12e447e41417515cb
Nishant kumar <nishants440@gmail.com>
2022-02-26 14:44:24 +0530

    JavaScript With Firebase

---
+++ Vanila JS/index.js
@@ -0,0 +1,84 @@
+const firebaseApp = firebase.initializeApp({
+    //Your own Firebase Credentials..
+});
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+const register = () => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((res) => {
+        console.log(res.user)
+    })
+    .catch((err) => {
+        alert(err.message)
+        console.log(err.code)
+        console.log(err.message)
+    })
+}
+
+const login = () => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then((res) => {
+        console.log(res.user)
+    })
+    .catch((err) => {
+        alert(err.message)
+        console.log(err.code)
+        console.log(err.message)
+    })
+}
+
+const saveData = () => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    db.collection('users')
+    .add({
+        email: email,
+        password: password
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+}
+
+const readData = () => {
+    db.collection('users')
+    .get()
+    .then((data) => {
+        console.log(data.docs.map((item) => {
+            return {...item.data(), id: item.id}
+        }))
+    })
+}
+
+const updateData = () => {
+    db.collection('users').doc('6caYOiNxwviOJFIQ4Uag')
+    .update({
+        email: 'ashishisagoodboy1234@gmail.com',
+        password: '123456'
+    })
+    .then(() => {
+        alert('Data Updated')
+    })
+}
+
+const deleteData = () => {
+    db.collection('users').doc('6caYOiNxwviOJFIQ4Uag').delete()
+    .then(() => {
+        alert('Data Deleted')
+    })
+    .catch((err) =>{
+        console.log(err)
+    })
+}
